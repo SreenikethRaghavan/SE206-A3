197d0fe7ce84099358d400c673d3016d3ccb7144
NoahJade <limetreefrog@gmail.com>
2019-10-26 12:32:17 +1300

    LearnMenuController Clean and comment

--- SE206_A3/src/controllers/LearnMenuController.java
+++ SE206_A3/src/controllers/LearnMenuController.java
@@ -25,73 +25,143 @@ import objects.Level;
  */
 public class LearnMenuController {
 
+	// level bar elements
 	@FXML
 	private ProgressBar xpProgressBar;
-
 	@FXML
 	private Text xpText;
-
 	@FXML
 	private Text lvlNumTxt;
+	
 
-	@FXML
-	private Button backButton;
-
+	// buttons to be put into an array
 	@FXML
 	private Button btn1;
-
 	@FXML
 	private Button btn2;
-
 	@FXML
 	private Button btn3;
-
 	@FXML
 	private Button btn4;
-
+	private Button[] gameBtns;
+	
+	
+	// lock images to be put into an array
 	@FXML
 	private ImageView lock1;
-
 	@FXML
 	private ImageView lock2;
-
 	@FXML
 	private ImageView lock3;
-
-	@FXML
-	private ImageView settingImageView;
-
 	@FXML
 	private ImageView lock4;
+	private ImageView[] locks;
 
+	
+	// settings pane elements
+	@FXML
+	private ImageView settingImageView;
 	@FXML
 	private AnchorPane settingsPane;
 	
-	//warning pane items
+	
+	// warning pane elements
 	@FXML
 	private AnchorPane warningPane;
-
 	@FXML
 	private Text availabilityText;
-
 	@FXML
 	private Text warningText;
+	
+	// other scene elements
+	@FXML
+	private Button backButton;
 
-
-	private ImageView[] locks;
-	private Button[] gameBtns;
-
-	//should have a "level" object that contains max xp, min xp, features unlocked.
+	// Scene variables to store the current level and XP of the user.
 	private Level currLevel;
-	private int currXP;
+	private int currXP; 
 
 
+	/*
+	 * This function sets up the scene, including checking for the current state of the application.
+	 * In order for the menu to be properly set up, it needs to know what level the user is currently
+	 * at, what games they have currently unlocked, and whether there are resources available to play
+	 * those games. By using helper functions, initialize() sets up the scene.
+	 */
+	@FXML
+	private void initialize() {
+		locks = new ImageView[] {lock1,lock2,lock3,lock4};
+		gameBtns = new Button[] {btn1,btn2,btn3,btn4};
+		settingsPane.setVisible(false);
+		warningPane.setVisible(false);
 
+		updateLevel();
+		String messageText = "";
+		String notAvailable ="";
+		
+		// now check if there are files to play
+		if(!checkForMemoryImages()) {
+			//there are no memory images yet.
+			gameBtns[1].setDisable(true);
+			messageText = "no memory images";
+			notAvailable = "memory game is not available.\n\nTo enable it creation a new creation.";
+		}
+		
+		if(!checkForQuizVideos()) {
+			gameBtns[0].setDisable(true);
+			if(messageText.length() != 0) {
+				messageText = "no memory images or quiz videos";
+				notAvailable = "memory game and the quiz game are not available.\n\nTo enable them creation a new creation.";
+			} else {
+				messageText = "no quiz videos";
+				notAvailable = "quiz game is not available.\n\nTo enable it creation a new creation.";
+			}
+		}
+		
+		if(messageText.length() != 0) {
+			warningPane.setVisible(true);
+			warningText.setText("There are currently "+messageText+".");
+			availabilityText.setText("As such, the "+notAvailable);
+		}
+
+	}
+	
+	// ------------------------------------------------------------------------------------------------------------------------------------
+	// NAVIGATION FUNCTIONS
+	
+    @FXML
+    void exitWarning(ActionEvent event) {
+    	warningPane.setVisible(false);
+    }
+
+    @FXML
+    void goToCreateCreation(ActionEvent event) throws IOException {
+    	AppWindow.valueOf("CreateMenu").setScene(event);
+    }
+
+	@FXML
+	private void goToQuiz(ActionEvent event) throws IOException {
+		AppWindow.valueOf("Quiz").setScene(event);
+	}
+
+	@FXML
+	private void goToMissingWord(ActionEvent event) throws IOException {
+		AppWindow.valueOf("Memory").setScene(event);
+	}
+	
 	@FXML
 	void exitSettings(ActionEvent event) {
 		settingsPane.setVisible(false);
-	}   
-
+	}  
+	
+	@FXML
+	private void returnToMainMenu(ActionEvent event) throws IOException {
+		AppWindow.valueOf("MainMenu").setScene(event);
+	}
+	
+	/*
+	 * Toggles whether the settings pane is visible or not.
+	 */
 	@FXML
 	void showSettings(ActionEvent event) {
 		if(settingsPane.isVisible()) {
@@ -99,9 +169,11 @@ public class LearnMenuController {
 		} else {
 			settingsPane.setVisible(true);
 		}
-
 	}
-
+	
+	// ------------------------------------------------------------------------------------------------------------------------------------
+	// HELPER FUNCTIONS
+	
 	@FXML
 	void resetXP(ActionEvent event) {
 		AssociationClass.getInstance().increaseXP(-AssociationClass.getInstance().getXP());
@@ -127,34 +199,34 @@ public class LearnMenuController {
 		locks[index].setVisible(false);
 		gameBtns[index].setDisable(false);
 	}
-
+	
+	/*
+	 * Gets all the relevant information about the current level and updates the GUI
+	 * to reflect this information.
+	 */
 	private void updateLevel() {
-		//find out what level we are at
-		//we will first have to get the current XP
-		//eventually this will be stored in the association class
-		//but for now lets have a dummy value
+		//To find out what level we are on we will first have to get the current XP
 		currXP = AssociationClass.getInstance().getXP();
 		AssociationClass.getInstance().saveProgess();
-		//then loop through the level enums, til we get to one that is the current level.
+		
 		for (Level level : Level.values()) {
-			//iterate through each level
+			//iterate through each level to find the level with the corresponding xp boundaries
 			if(level.isThisLevel(currXP)) {
 				currLevel = level;
 			}
 		}
-
+		
+		//if we dont find a level, then thats an error.
 		if(currLevel == null) {
 			System.out.println("Error: No level was found. Assigning default Lvl 2.");
 			currLevel = Level.L2;
 		}
 
-
 		//now that we have that level, we need to update the text.
 		lvlNumTxt.setText(currLevel.getLevelNumber());
 		xpText.setText(currLevel.getXPText(currXP));
 
-		//here we need to access the level, to find out whats currently available.
-		//and lock off other elements
+		//determine which games are unlocked, and which need to be locked.
 		for(int i =0; i < locks.length; i++) {
 			if(currLevel.isUnlocked(i)) {
 				unlockGame(i);
@@ -163,90 +235,13 @@ public class LearnMenuController {
 			}
 		}
 
-		//with the currXP we need to update the progress bar
+		//update the progress bar with the current XP
 		xpProgressBar.setProgress(currLevel.getProgress(currXP));
 	}
-
-	@FXML
-	private void initialize() {
-		locks = new ImageView[] {lock1,lock2,lock3,lock4};
-		gameBtns = new Button[] {btn1,btn2,btn3,btn4};
-		settingsPane.setVisible(false);
-		warningPane.setVisible(false);
-
-		updateLevel();
-		String messageText = "";
-		String notAvailable ="";
-		// now check if there are files to play
-		if(!checkForMemoryImages()) {
-			//there are no memory images yet.
-			gameBtns[1].setDisable(true);
-			messageText = "no memory images";
-			notAvailable = "memory game is not available.\n\nTo enable it creation a new creation.";
-		}
-		
-		if(!checkForQuizVideos()) {
-			gameBtns[0].setDisable(true);
-			if(messageText.length() != 0) {
-				messageText = "no memory images or quiz videos";
-				notAvailable = "memory game and the quiz game are not available.\n\nTo enable them creation a new creation.";
-			} else {
-				messageText = "no quiz videos";
-				notAvailable = "quiz game is not available.\n\nTo enable it creation a new creation.";
-			}
-		}
-		
-		if(messageText.length() != 0) {
-			warningPane.setVisible(true);
-			warningText.setText("There are currently "+messageText+".");
-			availabilityText.setText("As such, the "+notAvailable);
-			/*Alert noCreations = new Alert(Alert.AlertType.INFORMATION);
-			noCreations.setTitle("Games Limited");
-			noCreations.setHeaderText("There are currently "+messageText+".");
-			noCreations.setContentText("As such, the "+notAvailable);
-			noCreations.showAndWait();	*/
-		}
-
-	}
-
-    @FXML
-    void exitWarning(ActionEvent event) {
-    	warningPane.setVisible(false);
-    }
-
-    @FXML
-    void goToCreateCreation(ActionEvent event) throws IOException {
-    	AppWindow.valueOf("CreateMenu").setScene(event);
-    }
-
-	@FXML
-	private void goToQuiz(ActionEvent event) throws IOException {
-
-		//while this game isnt implemented, it will just go to the quiz game
-		if(checkForEmptyCreationList(event)) {
-			return;
-		}
-		AppWindow.valueOf("Quiz").setScene(event);
-
-	}
-
-	@FXML
-	private void goToMissingWord(ActionEvent event) throws IOException {
-		//while this game isnt implemented, it will just go to the quiz game
-
-		if(checkForEmptyCreationList(event)) {
-			return;
-		}
-
-		AppWindow.valueOf("Memory").setScene(event);
-	}
 	
-
-	@FXML
-	private void returnToMainMenu(ActionEvent event) throws IOException {
-		AppWindow.valueOf("MainMenu").setScene(event);
-	}
-	
+	/*
+	 * This helper function checks whether there are resources available for the quiz game
+	 */
 	private boolean checkForQuizVideos() {
 		File[] files = new File("./creation_files/quiz_files/quiz_images").listFiles();
 		if(files.length ==0) {
@@ -254,31 +249,16 @@ public class LearnMenuController {
 		}
 		return true;
 	}
-	
+
+	/*
+	 * This helper function checks whether there are resources available for the memory game
+	 */
 	private boolean checkForMemoryImages() {
 		File[] files = new File("./creation_files/memory_files").listFiles();
 		if(files.length ==0) {
 			return false;
 		}
 		return true;
-	}
-
-	@FXML
-	private boolean checkForEmptyCreationList(ActionEvent e) throws IOException {
-
-		File[] files = new File("./creation_files/quiz_files/quiz_images").listFiles();
-
-		if(files.length == 0) {
-			Alert noCreations = new Alert(Alert.AlertType.INFORMATION);
-			noCreations.setTitle("No Existing Creations");
-			noCreations.setHeaderText("There are currently no creations to quiz you on.");
-			noCreations.setContentText("Kindly quit and create a creation to unlock the quiz component. ");
-			noCreations.showAndWait();	
-
-			return true;
-		}
-
-		return false;
 	}
 
 }
